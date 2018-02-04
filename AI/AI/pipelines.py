d0153f0289c9b9806f9e292d3a598970104f0760
Your Name <lidongtaolk@sina.com>
2018-02-04 14:17:35 +0800

    AI2

--- AI/AI/pipelines.py
+++ AI/AI/pipelines.py
@@ -4,8 +4,15 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class AiPipeline(object):
+    def __init__(self):
+        self.filename=open("AI.json","wb")
+
     def process_item(self, item, spider):
+        jsontext=json.dumps(dict(item),ensure_ascii=False)+"\n"
+        self.filename.write(jsontext.encode("utf-8"))
         return item
+    def close_spider(self,spider):
+        self.filename.close()
